1b1981b0f1f84d9848d35f2187c5357ba7f3a855
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-09-19 09:25:15 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,67 @@
+import style from "./style.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [temp, setTemp] = useState("");
+  const [word, setWord] = useState("");
+  const [size, setSize] = useState(400);
+  const [bgColor, setBgColor] = useState("ffffff");
+  const [qrCode, setQrCode] = useState("");
+  useEffect(() => {
+    setQrCode(
+      `http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`
+    );
+  }, [word, size, bgColor]);
+  function handleClick() {
+    setWord(temp);
+  }
+  return (
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <div className="input-box">
+        <div className="gen">
+          <textarea
+            type="text"
+            onChange={(e) => {
+              setTemp(e.target.value);
+            }}
+            placeholder="Enter text to encode"
+          />
+          <div className="gen_button">
+            <button className="button" onClick={handleClick}>
+              Generate
+            </button>
+          </div>
+        </div>
+        <div className="extra">
+          <h5>Background Color:</h5>
+          <input
+            type="color"
+            className="input_color"
+            onChange={(e) => {
+              setBgColor(e.target.value.substring(1));
+            }}
+          />
+          <h5>Dimension:</h5>
+          <input
+            type="range"
+            min="200"
+            max="600"
+            value={size}
+            onChange={(e) => {
+              setSize(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <div className="output-box">
+        <img src={qrCode} alt="" />
+        <a href={qrCode} download="QRCode">
+          <button type="button">Download</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
